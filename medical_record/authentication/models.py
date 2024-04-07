46796858e346f1b7d2e659b789a7077da25025c4
NchourupouoM <nchourupouom04@gmail.com>
2024-04-07 12:24:51 +0100

    creation du model User et execution des premieres migrations

--- medical_record/authentication/models.py
+++ medical_record/authentication/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+
+class User(AbstractUser):
+    PATIENT = 'PATIENT'
+    MEDECIN = 'MEDECIN'
+    MASCULIN = 'M'
+    FEMININ = 'F'
+
+    ROLE_CHOICES_ONE = (
+        (PATIENT,'patient'),
+        (MEDECIN,'medecin'),
+    )
+
+    ROLE_CHOICES_TWO = (
+        (MASCULIN,'M'),
+        (FEMININ,'F'),
+    )
+    
+    profile_photo = models.ImageField(verbose_name='photo de profil')
+    role = models.CharField(max_length=30, choices=ROLE_CHOICES_ONE, verbose_name='rôle')
+    date_naissance = models.DateField()
+    sex = models.CharField(max_length=1,choices=ROLE_CHOICES_TWO,verbose_name='sexe')
+    telephone = models.IntegerField(null=True)
+
